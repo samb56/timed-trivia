9f3a040e2585bf28862da64c2ce5c9c61c70cfd1
Sam <2726-sambergeland-WKCcKe@users.noreply.uci.bootcampcontent.com>
2022-05-24 17:38:48 -0700

    added questions and various style changes

--- assets/script.js
+++ assets/script.js
@@ -1,48 +1,43 @@
 
-
-// create timer for quiz that begins with start button
-
+var quiz = document.getElementById("#quiz")
+var questionNumber = document.getElementById("#questionNumber")
+var questionText = document.getElementById("#questionText")
+var answer1 = document.getElementById("#answer1")
+var answer2 = document.getElementById("#answer2")
+var answer3 = document.getElementById("#answer3")
+var answer4 = document.getElementById("#answer4")
 var timeText = document.querySelector("#secondsRemaining");
-var timeStart = true
 var startButton = document.querySelector(".startButton")
 
 
 
 function countdown() {
   var remainingTime = 90
-  if (timeStart = !true) {
-    var interval = setInterval(function timer() {
-
-      if (remainingTime > 0) {
-
-        remainingTime = --remainingTime;
-        console.log(remainingTime);
-        timeText.textContent = remainingTime;
-
-
-
-      } else {
-        clearInterval(interval);
-      }
-    }, 1000);
-  }
+  var interval = setInterval(function timer() {
+    if (remainingTime > 0) {
+      remainingTime = --remainingTime;
+      console.log(remainingTime);
+      timeText.textContent = remainingTime;
+    } else {
+      clearInterval(interval);
+    }
+  }, 1000);
 }
+
 //start button
 startButton.addEventListener("click", function () {
-  if (timeStart = true) {
-    timeStart = !true;
-  } else {
-    timeStart = true;
-  }
-
+  countdown();
 
 })
 
 
-
-
-  //use guess input
-
-
 // create rotating questions with clickable answers
+
+let currentQuestion = 0
+
+function runQuestion() {
+  quiz.setAttribute("style= "" ")
+}
+
+
 // create high score tracker
